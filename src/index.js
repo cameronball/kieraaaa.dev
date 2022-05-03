bcfa9f5e83a1db0ac4a17257c26a0e364d694df0
Cameron <49366907+cameronball@users.noreply.github.com>
2022-05-03 15:47:20 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,17 +4,14 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
 )
-//res.redirect(301, 'https://kieraaaa.xyz/');
-//console.log('no dont includes domain_name in ', hostname);
-  
-
+// res.redirect(301, 'https://kieraaaa.xyz/');
+// console.log('no dont includes domain_name in ', hostname);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
