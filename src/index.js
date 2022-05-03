632b1515237c8ebe024368ce36decc5d1815dee5
Cameron <49366907+cameronball@users.noreply.github.com>
2022-05-03 16:02:51 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+var express = require('express');
+var app = express();
+var PORT = 80;
 
-const currentUrl = req.get("host");
-console.log(currentUrl)
+app.get('/', function (req, res) {
+    const currentUrl = req.get("host");
+    console.log(currentUrl)
+    ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
+    )
+    res.end();
+});
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-)
+
 // res.redirect(301, 'https://kieraaaa.xyz/');
 // console.log('no dont includes domain_name in ', hostname);
 
