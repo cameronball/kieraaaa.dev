07a34cf7fa34763ba9a640ae0ae97a9ed558489f
Cameron <49366907+cameronball@users.noreply.github.com>
2022-05-03 16:22:48 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-if (window.location.hostname == "kieraaaa.xyz") {
+if (window.location.hostname === 'kieraaaa.xyz') {
   ReactDOM.render(
   <React.StrictMode>
     <App />
