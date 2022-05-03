1eb2190b973ea8b19e315cc32805503787c9b9b6
Cameron <49366907+cameronball@users.noreply.github.com>
2022-05-03 15:54:16 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
+const currentUrl = req.get("host");
+console.log(currentUrl)
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
