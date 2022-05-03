a83a13b85ed3cc7a692bf169076ca3328429a065
Cameron <49366907+cameronball@users.noreply.github.com>
2022-05-03 16:37:59 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-const https = require('https')
+const http = require('http')
 
-const server = https.createServer(function(req, res) {
+const server = http.createServer(function(req, res) {
   if (req.hostname === 'kieraaaa.xyz') {
     ReactDOM.render(
     <React.StrictMode>
